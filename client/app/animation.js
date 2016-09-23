a414561a654e3e2f41559ab03da41e4964d0ee93
Eventures <Peter@cdws-mbp-2.mynet>
2016-09-23 18:59:07 +0100

    add modal

--- client/app/animation.js
+++ client/app/animation.js
@@ -5,11 +5,14 @@ window.addEventListener('load', function () {
     var paperfold = $('.paperfold').paperfold({
         duration: 300,
         folds: 1,
-        maxFoldHeight: 40,
+        //maxFoldHeight: 40,
         isOpen: true,
         topShadow: 'linear-gradient(transparent, rgba(169 ,169, 169, 0.4)',
         bottomShadow: 'linear-gradient(rgba(169 ,169, 169, 0.4), transparent)'
     });
 
     $('.menu').click(paperfold.toggle);
+
+
+
 });
--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,28 @@
 'use strict';
-
-$(window).on('load', function () {
-    $( '.work' ).load( 'app/work.html' );
-
-    $('.read-btn').click(function(event) {
-        event.target.parent.addClass('expanded');
+// TODO best practices JQuery
+function openImageModal(imageName) {
+    $('<div style="{width: 80vw}"><img src="/assets/images/' + imageName  + '.jpg" class="modal-img"></div>').appendTo('body').modal({
+        fadeDuration: 300,
+        fadeDelay: 0.90
     });
-    
+};
+
+$(document).ready(function () {
+
+    var toggleButtonText = function (element) {
+        var buttonText = element.html() === 'read more' ? 'close' : 'read more';
+        element.html(buttonText);
+    };
+
+    $('.work').load('app/work.html', function () {
+        $('.read-btn.polestar').click(function () {
+            $('.project-wrap.polestar').toggleClass('expanded');
+            toggleButtonText($('.read-btn.polestar'));
+        });
+    });
+
 });
+
+
 
 
