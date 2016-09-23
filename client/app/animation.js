43f1e5605cc9bdb1cbb5d786a0bb898819ec7df1
Eventures <Peter@cdws-mbp-2.mynet>
2016-09-23 23:51:53 +0100

    change navigation

--- client/app/animation.js
+++ client/app/animation.js
@@ -2,16 +2,7 @@
 
 
 window.addEventListener('load', function () {
-    var paperfold = $('.paperfold').paperfold({
-        duration: 300,
-        folds: 1,
-        //maxFoldHeight: 40,
-        isOpen: true,
-        topShadow: 'linear-gradient(transparent, rgba(169 ,169, 169, 0.4)',
-        bottomShadow: 'linear-gradient(rgba(169 ,169, 169, 0.4), transparent)'
-    });
 
-    $('.menu').click(paperfold.toggle);
 
 
 
--- client/app/app.js
+++ client/app/app.js
@@ -1,25 +1,53 @@
 'use strict';
 // TODO best practices JQuery
 function openImageModal(imageName) {
-    $('<div style="{width: 80vw}"><img src="/assets/images/' + imageName  + '.jpg" class="modal-img"></div>').appendTo('body').modal({
+    $('<div><img src="/assets/images/' + imageName + '.jpg" class="modal-img"></div>').appendTo('body').modal({
         fadeDuration: 300,
         fadeDelay: 0.90
     });
 };
 
+function toggleButtonText(element) {
+    var buttonText = element.html() === 'read more' ? 'close' : 'read more';
+    element.html(buttonText);
+};
+
+function navigate(section) {
+
+    $('.main').load('app/' + section + '.html', function () {
+        if (section === 'work') {
+            $('.read-btn.polestar').click(function () {
+                $('.project-wrap.polestar').toggleClass('expanded');
+                toggleButtonText($('.read-btn.polestar'));
+            });
+        }
+    });
+
+}
+
 $(document).ready(function () {
 
-    var toggleButtonText = function (element) {
-        var buttonText = element.html() === 'read more' ? 'close' : 'read more';
-        element.html(buttonText);
-    };
+    navigate('work');
 
-    $('.work').load('app/work.html', function () {
-        $('.read-btn.polestar').click(function () {
-            $('.project-wrap.polestar').toggleClass('expanded');
-            toggleButtonText($('.read-btn.polestar'));
-        });
+    $('.menu-link').on('click', function (event) {
+        event.preventDefault();
+        var section = $(event.target).data('section');
+        if (section !== 'contact') {
+            navigate(section);
+        }
     });
+
+    var paperfold = $('.paperfold').paperfold({
+        duration: 300,
+        folds: 1,
+        //maxFoldHeight: 40,
+        isOpen: true,
+        topShadow: 'linear-gradient(transparent, rgba(169 ,169, 169, 0.4)',
+        bottomShadow: 'linear-gradient(rgba(169 ,169, 169, 0.4), transparent)'
+    });
+
+    $('.paperfold').click(paperfold.toggle);
+
 
 });
 
